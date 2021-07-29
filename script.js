60c0d94cd6ce58f179faa7e466401bf425c5a0dd
Marvelle <marvelle.blake@gmail.com>
2021-07-29 22:11:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Aadya" && b=="1234")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
